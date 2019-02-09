0bc838ed7aed79587871faae4aca169f1bcd6da6
Mayoogh <mayoogh1997@gmail.com>
2019-02-09 12:12:50 +0530

    Added data reading method

--- app/src/main/java/com/example/girish/googlefirebaseledcontrol/MainActivity.java
+++ app/src/main/java/com/example/girish/googlefirebaseledcontrol/MainActivity.java
@@ -1,27 +1,56 @@
 package com.example.girish.googlefirebaseledcontrol;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button on;
-    Button off;
+    Button forward, backward, left, right;
+    TextView text_temp;
+    DatabaseReference myDB_Ref;
+    String status;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        on = (Button)findViewById(R.id.on);
-        off = (Button)findViewById(R.id.off);
+        forward = (Button)findViewById(R.id.forward_id);
+        backward = (Button)findViewById(R.id.backward_id);
+        left = (Button)findViewById(R.id.left_id);
+        right = (Button)findViewById(R.id.right_id);
+        text_temp = (TextView)findViewById(R.id.txt_temp_id);
 
-        on.setOnClickListener(new View.OnClickListener(){
+        myDB_Ref = FirebaseDatabase.getInstance().getReference();
+        myDB_Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                status = dataSnapshot.getValue(String.class);
+                text_temp.setText(status);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        forward.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -29,14 +58,30 @@ public class MainActivity extends AppCompatActivity {
                 myRef.setValue(1);
             }
         });
-        off.setOnClickListener(new View.OnClickListener(){
+        backward.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(0);
+                myRef.setValue(2);
             }
         });
+        left.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("S1");
+                myRef.setValue(3);
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("S1");
+                myRef.setValue(4);
+            }
+        });
+
     }
 }
-
