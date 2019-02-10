e60771fa9dc4ee3e500dfa1120e2b4073ba38b2b
Mayoogh <mayoogh1997@gmail.com>
2019-02-10 16:43:02 +0530

    Added Touch Listener Now value will only changed if the button is pressed

--- app/src/main/java/com/example/girish/googlefirebaseledcontrol/MainActivity.java
+++ app/src/main/java/com/example/girish/googlefirebaseledcontrol/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.girish.googlefirebaseledcontrol;
 
+import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -18,13 +20,14 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button forward, backward, left, right, topL, topR, bottomL, botoomR;
+    Button forward, backward, left, right, topL, topR, bottomL, bottomR;
     Switch speed;
     TextView text_temp;
     DatabaseReference myDB_Ref;
     String status;
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         right = (Button)findViewById(R.id.right_id);
         topL = (Button)findViewById(R.id.topL_id);
         topR = (Button)findViewById(R.id.topR_id);
-        botoomR = (Button)findViewById(R.id.backR_id);
+        bottomR = (Button)findViewById(R.id.backR_id);
         bottomL = (Button)findViewById(R.id.backL_id);
         speed = (Switch)findViewById(R.id.speed_id);
         text_temp = (TextView)findViewById(R.id.txt_temp_id);
@@ -53,72 +56,134 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        forward.setOnClickListener(new View.OnClickListener(){
+        forward.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(1);
-            }
-        });
-        backward.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(2);
-            }
-        });
-        left.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(3);
-            }
-        });
-        right.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(4);
-            }
-        });
-        topL.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(5);
-            }
-        });
-        topR.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(6);
-            }
-        });
-        bottomL.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(7);
-            }
-        });
-        botoomR.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("S1");
-                myRef.setValue(8);
-            }
-        });
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(1);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
 
+            }
+        });
+        backward.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(2);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
 
+            }
+        });
+        left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(3);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
+
+            }
+        });
+        right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(4);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
+
+            }
+        });
+        topL.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(5);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
+
+            }
+        });
+        topR.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(6);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
+
+            }
+        });
+        bottomL.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(7);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
+
+            }
+        });
+        bottomR.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(8);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("S1");
+                    myRef.setValue(0);
+                }
+                return true;
+
+            }
+        });
 
     }
 
