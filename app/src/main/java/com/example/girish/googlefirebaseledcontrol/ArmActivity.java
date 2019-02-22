e7aabbe606e70a1d0f7d2fa17e76bfb61636db62
Mayoogh <mayoogh1997@gmail.com>
2019-02-22 08:29:28 +0530

    Added min value for seekbar 2 3 4 5

--- app/src/main/java/com/example/girish/googlefirebaseledcontrol/ArmActivity.java
+++ app/src/main/java/com/example/girish/googlefirebaseledcontrol/ArmActivity.java
@@ -2,6 +2,7 @@ package com.example.girish.googlefirebaseledcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,7 +12,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class ArmActivity extends AppCompatActivity {
 
-    TextView seekbar1_txt, seekbar2_txt, seekbar3_txt, seekbar4_txt, seekbar5_txt;
+    TextView seekbar1_txt, seekbar2_txt, seekbar3_txt, seekbar4_txt, seekbar5_txt, length_edit;
     SeekBar seekbar1, seekbar2, seekbar3, seekbar4, seekbar5;
 
     @Override
@@ -33,7 +34,8 @@ public class ArmActivity extends AppCompatActivity {
         seekbar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                seekbar1_txt.setText("" + progress + "");
+                seekbar1_txt.setText(Integer.toString(progress + 0));
+//                seekbar1_txt.setText("" + progress + "");
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("Servo1");
                 myRef.setValue(seekbar1_txt.getText().toString());
@@ -52,9 +54,10 @@ public class ArmActivity extends AppCompatActivity {
         seekbar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                seekbar2_txt.setText("" + progress + "");
+//                seekbar2_txt.setText("" + progress + "");
+                seekbar2_txt.setText(Integer.toString(progress + 200));
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Servo2");
+                DatabaseReference myRef = database.getReference("Servo1");
                 myRef.setValue(seekbar2_txt.getText().toString());
             }
             @Override
@@ -71,9 +74,10 @@ public class ArmActivity extends AppCompatActivity {
         seekbar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                seekbar3_txt.setText("" + progress + "");
+                seekbar3_txt.setText(Integer.toString(progress + 400));
+//                seekbar3_txt.setText("" + progress + "");
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Servo3");
+                DatabaseReference myRef = database.getReference("Servo1");
                 myRef.setValue(seekbar3_txt.getText().toString());
             }
             @Override
@@ -90,9 +94,10 @@ public class ArmActivity extends AppCompatActivity {
         seekbar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                seekbar4_txt.setText("" + progress + "");
+                seekbar4_txt.setText(Integer.toString(progress + 600));
+//                seekbar4_txt.setText("" + progress + "");
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Servo4");
+                DatabaseReference myRef = database.getReference("Servo1");
                 myRef.setValue(seekbar4_txt.getText().toString());
             }
             @Override
@@ -109,9 +114,10 @@ public class ArmActivity extends AppCompatActivity {
         seekbar5.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                seekbar5_txt.setText("" + progress + "");
+                seekbar5_txt.setText(Integer.toString(progress + 800));
+//                seekbar5_txt.setText("" + progress + "");
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Servo5");
+                DatabaseReference myRef = database.getReference("Servo1");
                 myRef.setValue(seekbar5_txt.getText().toString());
             }
             @Override
@@ -125,5 +131,25 @@ public class ArmActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                );
     }
 }
