09c0893d1e85c86993ed6580b24e573573614cb8
Mayoogh <mayoogh1997@gmail.com>
2019-02-22 08:30:45 +0530

    Added IP

--- app/src/main/java/com/example/girish/googlefirebaseledcontrol/CameraActivity.java
+++ app/src/main/java/com/example/girish/googlefirebaseledcontrol/CameraActivity.java
@@ -2,6 +2,7 @@ package com.example.girish.googlefirebaseledcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -16,6 +17,24 @@ public class CameraActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webview_id);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://192.168.1.4:8081/");
+        webView.loadUrl("http://192.168.1.6:8081/");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                );
     }
 }
